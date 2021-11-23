f91a8644bd562c4ba4068359e7feff64d068f746
Maristela Peglow <marispeg@gmail.com>
2021-11-22 21:35:05 -0300

    adicionado tolowercase em todos objetos

--- js/script.js
+++ js/script.js
@@ -58,29 +58,27 @@ var oe = [
 
 
 $(document).ready(function() {
- // console.log(oe)
   
   
   $("#container_oe").append("Utilize este formulário para fazer sua pesquisa.");
   
   $("#pesq").on("click",function(){
-  //  console.log("clicado")
     var oleo = $("#oleo").val();
     var emocional = $("#emocional").val();
     var vibracional = $("#vibracional").val();
     var fisica = $("#fisica").val();
-   // console.log(oleo);
-   // console.log(emocional);
+   
+    
     $("#container_oe").html("");
     var oe_filtrado = oe.filter(function(objeto){
-      return objeto.nome.toLowerCase().includes(oleo.toLowerCase()) && objeto.propriedades_emocionais.includes(emocional) 
-      && objeto.propriedades_vibracionais.includes(vibracional) && objeto.propriedades_fisicas.includes(fisica); 
+      return objeto.nome.toLowerCase().includes(oleo.toLowerCase()) && objeto.propriedades_emocionais.toLowerCase().includes(emocional.toLowerCase()) 
+      && objeto.propriedades_vibracionais.toLowerCase().includes(vibracional.toLowerCase()) && objeto.propriedades_fisicas.toLowerCase().includes(fisica.toLowerCase()); 
       
     });
     if (oe_filtrado.length == 0 ){
       $("#container_oe").append("Nenhum resultado encontrado");
     }
-   // console.log(oe_filtrado)
+    
     $.each(oe_filtrado,function(i,item){
       $("#container_oe").append("<div><h5>"+item.nome+"</h5><br>"+"<h6>Propriedades Emocionais:</h6>"+item.propriedades_emocionais+"<br>"+"<h6>Propriedades Vibracionais:</h6>"+
       item.propriedades_vibracionais+"<br>"+"<h6>Propriedades Físicas:</h6>"+item.propriedades_fisicas+"<br>"+"<h6>Precauções:</h6>"+item.precaucoes+"</div>");
